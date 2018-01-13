4f01714aef047ad3bcbaf92f3e5d9573b67cfc50
shinchan2020 <33303639+shinchan2020@users.noreply.github.com>
2018-01-13 21:47:31 +0700

    Create sett.js

---
+++ sett.js
@@ -0,0 +1,50 @@
+function labelset1(e) {
+    var a = e.entry,
+        t = a.category[1].term;
+  document.write(&#39;&#39; + t + &quot;&quot;)
+};
+function labelset2(e) {
+    var a = e.entry,
+        t = a.category[2].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset3(e) {
+    var a = e.entry,
+        t = a.category[3].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset4(e) {
+    var a = e.entry,
+        t = a.category[4].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset5(e) {
+    var a = e.entry,
+        t = a.category[5].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset6(e) {
+    var a = e.entry,
+        t = a.category[6].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset7(e) {
+    var a = e.entry,
+        t = a.category[7].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset8(e) {
+    var a = e.entry,
+        t = a.category[8].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset9(e) {
+    var a = e.entry,
+        t = a.category[9].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
+function labelset10(e) {
+    var a = e.entry,
+        t = a.category[10].term;
+  document.write(&#39;,&#39; + t + &quot;&quot;)
+};
